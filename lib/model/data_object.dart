18507133b6152af04d63babfbee5fd4db8d73098
Aatika Khan <aatikakhan869@gmail.com>
2022-08-03 20:43:33 +0530

    data object nullsafety

--- lib/model/data_object.dart
+++ lib/model/data_object.dart
@@ -19,26 +19,13 @@ class DataObject {
       this.url});
 
   DataObject.fromJson(Map<String, dynamic> json) {
-    copyright = json['copyright'];
-    date = json['date'];
-    explanation = json['explanation'];
+    copyright = json['copyright'] ?? '';
+    date = json['date'] ?? '';
+    explanation = json['explanation'] ?? '';
     hdurl = json['hdurl'];
-    mediaType = json['media_type'];
-    serviceVersion = json['service_version'];
-    title = json['title'];
-    url = json['url'];
-  }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data =  <String, dynamic>{};
-    data['copyright'] = copyright;
-    data['date'] = date;
-    data['explanation'] = explanation;
-    data['hdurl'] = hdurl;
-    data['media_type'] = mediaType;
-    data['service_version'] = serviceVersion;
-    data['title'] = title;
-    data['url'] = url;
-    return data;
+    mediaType = json['media_type'] ?? '';
+    serviceVersion = json['service_version'] ?? '';
+    title = json['title'] ?? '';
+    url = json['url'] ?? '';
   }
 }
